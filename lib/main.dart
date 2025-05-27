1ff6ec6008a4fb6ae8afa6277383eb4dc459267d
IbrahimOraby <orabyibrahim19@gmail.com>
2025-05-27 05:01:02 +0300

    feat: add splash screen
    - install and import flutter_native_splash package
    - create assets/images
    - apply styles for light and dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:clutch_sports_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -9,6 +12,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MaterialApp(
       title: 'Clutch',
       theme: ThemeData(),
