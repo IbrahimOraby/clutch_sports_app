00985c70d4781850f210eb3cbee9afb125f02743
IbrahimOraby <orabyibrahim19@gmail.com>
2025-05-28 05:00:09 +0300

    feat: implement onboarding screen for first-time users
    - install smooth_page_indicator package
    - import onboarding images
    - create onboarding screen
    - install shared_preferences package
    - use onBoardingVisited pref

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
 import 'package:clutch_sports_app/screens/home_screen.dart';
+import 'package:clutch_sports_app/screens/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(const MyApp());
+
+  final prefs = await SharedPreferences.getInstance();
+  final onBoardingVisited = prefs.getBool('onBoardingVisited') ?? false;
+  runApp(MyApp(onBoardingVisited: onBoardingVisited));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool onBoardingVisited;
+
+  const MyApp({super.key, required this.onBoardingVisited});
   @override
   Widget build(BuildContext context) {
     FlutterNativeSplash.remove();
     return MaterialApp(
       title: 'Clutch',
-      theme: ThemeData(),
-      home: const HomeScreen(),
+      theme: ThemeData(
+        textTheme: TextTheme(
+          headlineMedium: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.normal,
+            color: Colors.grey[800], // or Colors.black if you prefer
+          ),
+        ),
+      ),
+      home: onBoardingVisited ? HomeScreen() : OnboardingScreen(),
     );
   }
 }
---
+++ lib/screens/onboarding.dart
@@ -0,0 +1,140 @@
+import 'package:clutch_sports_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          //swipeable pages
+          PageView(
+            controller: _pageController,
+            children: [
+              _buildOnboardingPage(
+                context,
+                image: 'assets/images/onboarding-1.png',
+                title: 'All Your Favorite Teams in One Place',
+                description:
+                    'Follow matches, leagues, and players from around the world.',
+              ),
+              _buildOnboardingPage(
+                context,
+                image: 'assets/images/onboarding-2.png',
+                title: 'Browse by Country & League',
+                description:
+                    'Choose your favorite teams, countries, or leagues to follow.',
+              ),
+              _buildOnboardingPage(
+                context,
+                image: 'assets/images/onboarding-3.png',
+                title: 'Discover Teams Around the Globe',
+                description:
+                    'Explore leagues from Europe to South America. Dive into team stats, match schedules, and more.',
+              ),
+            ],
+          ),
+
+          //SKIP
+          Positioned(
+            top: kToolbarHeight,
+            right: 24,
+            child: TextButton(
+              onPressed: () async {
+                //set onBoardingVisited to true
+                final prefs = await SharedPreferences.getInstance();
+                prefs.setBool('onBoardingVisited', true);
+
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              },
+              child: const Text(
+                'SKIP',
+                style: TextStyle(color: Color(0xFF578FCA)),
+              ),
+            ),
+          ),
+
+          //indicator
+          Positioned(
+            bottom: kBottomNavigationBarHeight + 25,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: SmoothPageIndicator(
+                controller: _pageController,
+                effect: const ExpandingDotsEffect(
+                  activeDotColor: Color(0xFF578FCA),
+                  dotHeight: 6,
+                ),
+                count: 3,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildOnboardingPage(
+    BuildContext context, {
+    required String image,
+    required String title,
+    required String description,
+  }) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // image
+          SizedBox(
+            height: screenHeight * 0.3,
+            child: Center(
+              child: Image.asset(
+                image,
+                width: MediaQuery.sizeOf(context).width * 0.8,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          SizedBox(height: 24),
+          // title
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+
+          SizedBox(height: 12),
+          // description
+          Text(
+            description,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
